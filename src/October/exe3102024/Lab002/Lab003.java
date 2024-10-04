0d07fe7fba3fd8ce5d09a505d5a43eff836ccb74
Sumaa Heroor <heroorsuma@gmail.com>
2024-10-04 06:46:15 +0530

    All files

---
+++ src/October/exe3102024/Lab002/Lab003.java
@@ -0,0 +1,10 @@
+package October.exe3102024.Lab002;
+
+public class Lab003 {
+    public static void main(String[] args){
+    System.out.println("Simple Calculator");
+    System.out.println("1+1=2");
+    System.out.println("3+4=7");
+    System.out.println("6+2=8");
+    }
+}
